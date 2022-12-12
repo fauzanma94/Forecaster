7fd36f4488c040bd2aecadfad404bb66c99d4d5c
trixtin23 <11201018@student.itk.ac.id>
2022-12-12 19:15:41 +0800

    test

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import './App.css';
 import Navbar from './Components/navbar';
 import SearchArticle from './Components/searchartikel';
 
-
 function App() {
   return(
       <React.Fragment>
-       
         <Navbar/>
         <SearchArticle/>
       </React.Fragment>
